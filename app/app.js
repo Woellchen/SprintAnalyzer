1ddcbf8da9c9cbc7b5efb0076a00809358610f48
Patrick Schmidt <patrick.schmidt@innogames.de>
2014-02-04 19:37:58 +0100

    first working version, showing labels for all completed issues for the last active sprint

--- app/app.js
+++ app/app.js
@@ -1,3 +1,33 @@
 var config = require('app/config.js');
+JiraApi = require('jira').JiraApi;
 
-console.log('works');
+var jira = new JiraApi('https', config.host, null, config.user, config.password, 'latest', true, true);
+
+jira.findRapidView('Forge of Empires', function(error, rapidView) {
+	if (error) {
+		throw error;
+	}
+
+	jira.getAllSprintsForRapidView(rapidView.id, function(error, sprints) {
+		if (error) {
+			throw error;
+		}
+
+		var lastActiveSprint = null;
+		while (lastActiveSprint = sprints.pop()) {
+			if (lastActiveSprint.state === 'CLOSED') {
+				break;
+			}
+		}
+
+		jira.getSprintIssues(rapidView.id, lastActiveSprint.id, function(error, issues) {
+			for (var i in issues.contents.completedIssues) {
+				var completedIssue = issues.contents.completedIssues[i];
+
+				jira.findIssue(completedIssue.id, function(error, data) {
+					console.log('labels for %s: %s', data.key, data.fields.labels);
+				});
+			}
+		});
+	});
+});
