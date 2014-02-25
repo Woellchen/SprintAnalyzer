53fa5b69d3c43de294a73b183e7fc5081d7b5c7a
Stephan Vock <stephan.vock@innogames.de>
2014-02-25 23:52:53 +0100

    added project specific sprint blacklist config moved get sprints method to Jira model because I need it later

--- app/handlers/ListSprintsHandler.js
+++ app/handlers/ListSprintsHandler.js
@@ -3,23 +3,9 @@ var jira = require('app/models/Jira.js');
 var listSprints = function(request, reply) {
 	var projectId = request.params.projectId;
 
-	jira.getProject(projectId, function(error, project) {
-		jira.findRapidView(project.name, function(error, rapidView) {
-			jira.getAllSprintsForRapidView(rapidView.id, function(error, sprints) {
-				jira.getBacklogForRapidView(rapidView.id, function(error, data) {
-					while (data.sprints.length) {
-						var backlogSprint = data.sprints.pop();
-						if (backlogSprint.state !== 'FUTURE') {
-							continue;
-						}
-
-						sprints.push(backlogSprint);
-					}
-
-					reply(sprints).type('application/json');
-				});
-			});
-		});
+	jira.getSprintsForProject(projectId, function (error, sprints) {
+		if (error) throw error; // maybe we should start thinking about error handling :D
+		reply(sprints).type('application/json');
 	});
 };
 
